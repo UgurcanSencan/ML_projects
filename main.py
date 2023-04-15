3127daf3bcc7c66c65792d18e426ddfd8c41b6f5
UgurcanSencan <ugurcansencan1408@gmail.com>
2023-04-15 15:07:09 +0300

    create python file for object detection app

---
+++ main.py
@@ -0,0 +1,10 @@
+from imageai.Detection import ObjectDetection
+
+model = ObjectDetection()
+
+path_model = "./Models/m1.h5"
+path_input = "./Input/images.jpg"
+path_output = "./Output/newimage.jpg"
+
+
+
