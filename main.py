9a6f0afc4824bd7395187b5acee450e10b7fcbcb
UgurcanSencan <ugurcansencan1408@gmail.com>
2023-04-15 15:09:45 +0300

    build a model and input/output path are added

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ path_model = "./Models/m1.h5"
 path_input = "./Input/images.jpg"
 path_output = "./Output/newimage.jpg"
 
+model.setModelTypeAsYOLOv3()
+model.setModelPath(path_model)
+model.loadModel()
+recognition = model.detectObjectsFromImage(
+    input_image=path_input,
+    output_image_path = path_output
+)
 
+for eachItem in recognition:
+    print(eachItem["name"], ":", eachItem["percentage_probability"])
 
